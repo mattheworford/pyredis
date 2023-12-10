47815b4d8325b144bc251f0b943ab6088d8cadea
Matthew Orford <mattorford9@gmail.com>
2023-12-09 22:43:40 -0500

    clean up protocol handler

--- src/protocol_handler.py
+++ src/protocol_handler.py
@@ -6,81 +6,67 @@ from src.models.protocol.error import Error
 from src.models.protocol.integer import Integer
 
 TERMINATOR_SEQUENCE = b"\r\n"
+TERMINATOR_SIZE = len(TERMINATOR_SEQUENCE)
 
 
 def extract_data_from_payload(payload):
-    match chr(payload[0]):
-        case '+':
-            return extract_simple_string_from_payload(payload)
-        case '-':
-            return extract_error_from_payload(payload)
-        case ':':
-            return extract_integer_from_payload(payload)
-        case '$':
-            return extract_bulk_string_from_payload(payload)
-        case '*':
-            return extract_array_from_payload(payload)
+    terminator_start = payload.find(TERMINATOR_SEQUENCE)
+    if terminator_start == -1:
+        return None, 0
+    else:
+        data = payload[1:terminator_start].decode()
+        remainder = payload[terminator_start + TERMINATOR_SIZE:]
+        match chr(payload[0]):
+            case '+':
+                return SimpleString(data), terminator_start + TERMINATOR_SIZE
+            case '-':
+                return Error.from_string(data), terminator_start + TERMINATOR_SIZE
+            case ':':
+                return parse_integer_and_size(data, terminator_start)
+            case '$':
+                return parse_bulk_string_and_size(data, remainder, terminator_start)
+            case '*':
+                return parse_array_and_size(data, remainder, terminator_start)
+        return None, 0
+
+
+def parse_integer_and_size(data, terminator_start):
+    integer = Integer.from_string(data)
+    if integer is not None:
+        return integer, terminator_start + TERMINATOR_SIZE
     return None, 0
 
 
-def extract_simple_string_from_payload(payload):
-    terminator = payload.find(TERMINATOR_SEQUENCE)
-    if terminator != -1:
-        return SimpleString(payload[1:terminator].decode()), terminator + len(TERMINATOR_SEQUENCE)
-    return None, 0
-
-
-def extract_error_from_payload(payload):
-    terminator = payload.find(TERMINATOR_SEQUENCE)
-    if terminator != -1:
-        return Error.from_string(payload[1:terminator].decode()), terminator + len(TERMINATOR_SEQUENCE)
-    return None, 0
-
-
-def extract_integer_from_payload(payload):
-    terminator = payload.find(TERMINATOR_SEQUENCE)
-    if terminator != -1:
-        integer = Integer.from_string(payload[1:terminator].decode())
-        if integer is not None:
-            return integer, terminator + len(TERMINATOR_SEQUENCE)
-    return None, 0
-
-
-def extract_bulk_string_from_payload(payload):
+def parse_bulk_string_and_size(length, remainder, terminator_start):
     try:
-        first_terminator_start = payload.find(TERMINATOR_SEQUENCE)
-        if first_terminator_start != -1:
-            length = int(payload[1:first_terminator_start].decode())
-            if length == -1:
-                return None, 5
-            first_terminator_end = first_terminator_start + len(TERMINATOR_SEQUENCE)
-            second_terminator = payload[first_terminator_end:].find(TERMINATOR_SEQUENCE)
-            if second_terminator != -1 and length == second_terminator:
-                data = payload[first_terminator_end:first_terminator_end + second_terminator].decode()
-                size = first_terminator_end + second_terminator + len(TERMINATOR_SEQUENCE)
-                return BulkString(data), size
+        length = int(length)
+        if length == -1:
+            return None, 5
+        first_terminator_end = terminator_start + TERMINATOR_SIZE
+        second_terminator = remainder.find(TERMINATOR_SEQUENCE)
+        if second_terminator != -1 and length == second_terminator:
+            data = remainder[:second_terminator].decode()
+            size = first_terminator_end + second_terminator + TERMINATOR_SIZE
+            return BulkString(data), size
     except ValueError:
         return None, 0
     return None, 0
 
 
-def extract_array_from_payload(payload):
+def parse_array_and_size(number_of_elements, remainder, terminator_start):
     try:
-        terminator = payload.find(TERMINATOR_SEQUENCE)
-        if terminator != -1:
-            number_of_elements = int(payload[1:terminator].decode())
-            if number_of_elements == -1:
-                return None, 5
-            elements, size = [], terminator + len(TERMINATOR_SEQUENCE)
-            for _ in range(number_of_elements):
-                if size >= len(payload)-1:
-                    return None, 0
-                curr_element, curr_size = extract_data_from_payload(payload[size:])
-                if curr_size == 0:
-                    return None, 0
-                elements.append(curr_element)
-                size += curr_size
-            return Array(elements), size
+        number_of_elements = int(number_of_elements)
+        if number_of_elements == -1:
+            return None, 5
+        elements, size = [], 0
+        for _ in range(number_of_elements):
+            if size >= len(remainder) - 1:
+                return None, 0
+            curr_element, curr_size = extract_data_from_payload(remainder[size:])
+            if curr_size == 0:
+                return None, 0
+            elements.append(curr_element)
+            size += curr_size
+        return Array(elements), size + terminator_start + TERMINATOR_SIZE
     except ValueError:
         return None, 0
-    return None, 0
