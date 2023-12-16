42a5a606b28e20c92024fb80bdfbee83df9048ea
Matthew Orford <mattorford9@gmail.com>
2023-12-16 15:37:22 -0500

    add passive expiry

--- src/models/data_store.py
+++ src/models/data_store.py
@@ -1,16 +1,25 @@
+from datetime import datetime
 from threading import Lock
-from typing import Any
+
+from src.models.entry import Entry
 
 
 class DataStore:
     def __init__(self) -> None:
-        self._data: dict[str, Any] = dict()
+        self._data: dict[str, Entry] = dict()
         self._lock = Lock()
 
-    def __getitem__(self, key: str) -> Any:
+    def __getitem__(self, key: str) -> Entry:
         with self._lock:
+            entry = self._data[key]
+            if entry.expiry is not None and entry.expiry <= datetime.now():
+                del self._data[key]
             return self._data[key]
 
-    def __setitem__(self, key: str, value: Any) -> None:
+    def __setitem__(self, key: str, entry: Entry) -> None:
         with self._lock:
-            self._data[key] = value
+            self._data[key] = entry
+
+    def __delitem__(self, key: str) -> None:
+        with self._lock:
+            del self._data[key]
